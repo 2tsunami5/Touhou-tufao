36d944be14769261f307256b8a9aab960fcf1685
2tsunami5 <albertsc_2@hotmail.com>
2025-11-17 12:38:23 -0300

    Initial Commit

---
+++ patch.js
@@ -0,0 +1,17 @@
+{
+    "dependencies": [
+        "nmlgc/base_tsa",
+        "nmlgc/base_tasofro",
+        "nmlgc/script_latin",
+        "nmlgc/western_name_order"
+    ],
+    "id": "lang_pt-br-tufão",
+    "servers": [
+        "https://srv.thpatch.net/lang_pt-br-tufão/"
+    ],
+    "title": "Tufão Team Translation",
+    "update": true
+    ],
+    "fonts": {
+        "ARIAL.ttf": true
+}
