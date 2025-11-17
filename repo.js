af39a2f8db7614c6c6363452dd4fa4272ff41f56
2tsunami5 <albertsc_2@hotmail.com>
2025-11-17 14:07:51 -0300

    Update Commit

--- repo.js
+++ repo.js
@@ -2,7 +2,7 @@
     "contact": "tufaoguinhoe@gmail.com",
     "id": "Tufão",
     "patches": {
-        "lang_pt-br-tufao": "Tradução PT-BR (Tradução da Tufão)"
+        "lang_pt-br-tufão": "Tradução PT-BR (Tradução da Tufão)"
     },
     "servers": [
         "https://mirrors.thpatch.net/Touhou-6_PTBR-tufao/lang_pt-br-tufão/"
