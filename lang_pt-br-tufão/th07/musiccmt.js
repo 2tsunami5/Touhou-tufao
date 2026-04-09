9acdf850091ef6d5d2030a9f87b9d201945b6661
2tsunami5 <albertsc_2@hotmail.com>
2026-04-09 10:05:18 -0300

    Add files via upload

---
+++ lang_pt-br-tufão/th07/musiccmt.js
@@ -0,0 +1,164 @@
+﻿{
+    "1": [
+        "@",
+        "　Tema de abertura.",
+        "　Mantendo a temática Japonesa do título anterior, dando um foco ainda maior a ela. Essa",
+        "　composição remove por completo aquela sensação 「está na hora da Chuva de Danmaku!」.",
+        "　Sendo honesto, estaria bom o bastante se essa composição musical existisse ou não,",
+        "　Aqui sua existência é para acalmar o espírito exaltado do Danmaku (*risos*)",
+    ],
+    "2": [
+        "@",
+        "　Tema do Estágio 1.",
+        "　De acordo com o título da música (algo que diz existir, não existe), imaginei uma natureza",
+        "　intocada. Para mim, estranhamente, acabou tendo um lado bem consolador.",
+        "　Por causa disso, para não pesar no sentimento consolador, encerrei com um tom leve.",
+        "　No entanto, de revigorante não tem absolutamente nada (^^;"
+    ],
+    "3": [
+        "@",
+        "　Tema da Letty Whiterock.",
+        "　(Não apenas nesta música) Sempre viso em minhas composições um misto entre o estilo Japonês",
+        "　e Ocidental. Acaba sendo curta por ser o Estágio 1, servindo mais como uma porta de",
+        "　entrada para o universo deste jogo. Tentei me expressar conforme as ideias apareciam",
+        "　em minha mente. Um som mais direto que, por incrível que pareça, não se encaixa em nenhum",
+        "　gênero musical."
+    ],
+    "4": [
+        "@",
+        "　Tema do Estágio 2.",
+        "　Esta aqui é uma... música bem esquisita. Pois é.",
+        "　Minha intenção era uma composição de alta influência folclórica, mas acabou ficando num calmo",
+        "　bem meia-boca. Influência folclórica. Como o próprio título diz, inspirada na região de Tōno.",
+        "　Minha intenção foi compor uma melodia nostálgica que evoca um",
+        "　Japão de outros tempos, que já não existe mais."
+    ],
+    "5": [
+        "@",
+        "　Tema da Chen.",
+        "　Compus com uma garota Yōkai energética em mente. Um ad-lib feito sem qualquer hesitação.",
+        "　De uma forma bem direta. Se eu tentasse inventar moda demais, acabaria saindo muito estranho.",
+        "　Era bem evidente que isso iria acontecer (*suando*). Por isso, resolvi compor com uma melodia",
+        "　tocada no improviso. Tudo isso feito após experimentar uma grande quantidade de álcool...",
+        "　Apesar dos pesares, é uma composição bem honesta comigo mesmo(^^;"
+    ],
+    "6": [
+        "@",
+        "　Tema do Estágio 3. De repente, já no último Estágio? É essa a sensação que esta música passa.",
+        "　Para falar a verdade, como estamos lançando esta versão de demonstração antecipadamente,",
+        "　não queria que parecesse algo feito pela metade. Resolvi trazer o primeiro momento decisivo do",
+        "　jogo para o Estágio 3. Por causa disso, decidi fazer uma composição um pouco mais séria desta",
+        "　vez. Mesmo eu dizendo isso, para falar a verdade, essa composição parece bem o eu dos velhos",
+        "　tempos; talvez notou isso. Provavelmente sou só eu, mas a música traz uma sensação nostálgica.",
+    ],
+    "7": [
+        "@",
+        "　Tema da Alice Margatroid.",
+        "　Alice é uma personagem um tanto especial na franquia, então estava motivado na composição",
+        "　desta música (falando dessa forma, parece que as outras composições não tiveram dedicação).",
+        "　Passar a sensação de uma luta crítica, mas ao mesmo tempo em que ambos os lados continuam",
+        "　mantendo sua compostura. É o que pretendo alcançar. Compostura é a própria essência da",
+        "　virtude das garotas... (É virtude) não é o visual Gótica Lolita (*risos*)"
+    ],
+    "8": [
+        "@",
+        "　Tema do Estágio 4. Estilo mais para o Chinês do que o Japonês. Um lugar radical, fascinante,",
+        "　acima das nuvens. Começando do Estágio 4, temos o verdadeiro começo do",
+        "　Youyoumu (Ilusão Misteriosa). Somente este Estágio acabou ficando absurdamente longo. Antes,",
+        "　uma música tão intensa quanto comovente que vai além da estratosfera (não tão alta assim)",
+        "　será que tivemos uma invasão deste nível?",
+        "　Compus com 'Jornada ao Oeste' em mente, apesar de não haver nenhuma semelhança."
+    ],
+    "9": [
+        "@",
+        "　Tema das 3 Irmãs Prismriver.",
+        "　De qualquer forma, a intenção era uma performance musical alegre e divertida, como se elas",
+        "　estivessem tocando seus instrumentos. Uma performance musical carregada pela paixão de três",
+        "　irmãs despreocupadas, que adoram um festival e agitação. Pelo menos essa era a intenção.",
+        "　No refrão, as três com os seus violinos, trompetes e teclados estão tocando",
+        "　como bem entendem. Uma verdadeira miscelânea."
+    ],
+    "10": [
+        "@",
+        "　Tema do Estágio 5.",
+        "　Evoca boas lembranças enraizadas do povo japonês com um pequeno alguém se aproximando.",
+        "　Tentei passar uma sensação de inquietação.",
+        "　Sons distantes parecidos com um Shō, junto ao toque de sinos, eram como o Outro Mundo soava",
+        "　em minha mente. ou só eu que penso assim?"
+    ],
+    "11": [
+        "@",
+        "　Tema da Youmu Konpaku.",
+        "　Tentei passar a sensação de uma oponente formidável + juvenil nesta música. Talvez seja a que",
+        "　mais combina com um chefão. Mesmo utilizando uma guitarra, desde o princípio abandonei a ideia",
+        "　de fazer rock (*risos*). A melodia deste refrão faz lembrar uma Yōkai."
+    ],
+    "12": [
+        "@",
+        "　Tema do Último Estágio.",
+        "　Para dar ênfase ao chefe do Último Estágio, o caminho até lá serve apenas como conexão.",
+        "　Uma música extremamente curta, mas que é bem tranquila e gostosa (acho).",
+        "　Para este Estágio, imaginei como se estivesse correndo sobre um mar de flores de cerejeiras.",
+        "　Mas será que a música conseguiu passar esta mesma impressão?"
+    ],
+    "13": [
+        "@",
+        "　Tema da Yuyuko Saigyouji.",
+        "　De qualquer forma, optei mais por uma melodia Oriental para algo como o desfecho.",
+        "　Eu falo bonito dessa forma, mas não importa como você ouça, no final das contas é a minha",
+        "　música de sempre (*risos*). Espero que tenha sido uma música que passe várias emoções",
+        "　ao mesmo tempo: intensa, elegante, vigorosa e bem delicada."
+    ],
+    "14": [
+        "@",
+        "　Tema da ???.",
+        "　Um arranjo do tema da Yuyuko.",
+        "　Desta vez, a atmosfera está um pouco mais misteriosa.",
+        "　A música em si é intensa a ponto de ficar cansado (*risos*)"
+    ],
+    "15": [
+        "@",
+        "　Tema do Estágio Extra.",
+        "　Uma música de ritmo acelerado, algo bem raro em Touhou (ou não?).",
+        "　Como dá para perceber vendo este Estágio, a ideia principal",
+        "　foi focar no tempo e no ritmo.",
+        "　A melodia acaba dando uma mudada de 112º e a partir disso é onde o Extra começa.",
+        "　Pense como se fosse o começo de tudo (*risos*)"
+    ],
+    "16": [
+        "@",
+        "　Tema da Ran Yakumo.",
+        "　Compus com uma imagem mais deslumbrante do que misteriosa.",
+        "　Espero que essa música passe a ideia da viveza que as Yōkais têm.",
+        "　Acho que no gênero Chuva de Balas, em batalhas longas, o rumo é ter muita repetição de",
+        "　um tom constante, então resolvi compor a música para puxar essa impressão."
+    ],
+    "17": [
+        "@",
+        "　Tema da ???.",
+        "　Outra versão do tema 'Spiritual Domination'.",
+        "　A música quase não tem mudanças..."
+    ],
+    "18": [
+        "@",
+        "　Tema da Yukari Yakumo.",
+        "　A intenção deste tema era para ter sido um arranjo da música da Ran.",
+        "　Nesta aqui, misturei um pouco de uma sensação ameaçadora.",
+        "　Digo, a personagem em si carrega uma aura de ameaça, algo bastante diferente",
+        "　das outras personagens introduzidas até agora, uma aparência suspeita e um Danmaku fora",
+        "　do comum. Como é a música de uma personagem assim, ela é bem esquisita (*risos*)"
+    ],
+    "19": [
+        "@",
+        "　Tema de Encerramento.",
+        "　...eu falei que é esquisita.",
+        "　",
+        "　É o dia a dia bem normal, sabe."
+    ],
+    "20": [
+        "@",
+        "　Tema dos Créditos Finais.",
+        "　Esta música é inspirada na cantiga infantil 「Sakura, Sakura」.",
+        "　Acabou sendo uma música bastante relaxante."
+    ]
+}
